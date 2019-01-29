6e7e3b121c4ca08f38cb87f051784194ee7500e6
abhijeet1410 <42213151+abhijeet1410@users.noreply.github.com>
2019-01-29 14:09:29 +0530

    Initial commit

---
+++ app/src/main/java/com/abhijeet14/firebasefacebookdemo/MainActivity.java
@@ -0,0 +1,103 @@
+package com.abhijeet14.firebasefacebookdemo;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private TextView textView2,textView3;
+    private ImageView dp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+            FacebookSdk.sdkInitialize(getApplicationContext());
+            loginButton = findViewById(R.id.login_button);
+
+            callbackManager = CallbackManager.Factory.create();
+            loginButton.setReadPermissions(Arrays.asList("email"));
+    }
+
+    public void buttonClickedFbLogin(View v){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "User cancelled it", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void handleFacebookToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent i = new Intent(getApplicationContext(),ProfileActivity.class);
+                            startActivity(i);
+                            finish();
+                        }else {
+                            Toast.makeText(MainActivity.this, "Couldn't register to Firebase", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/abhijeet14/firebasefacebookdemo/ProfileActivity.java
@@ -0,0 +1,71 @@
+package com.abhijeet14.firebasefacebookdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    TextView t1,t2;
+    ImageView userDP;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        t1 = findViewById(R.id.textEmail);
+        t2 = findViewById(R.id.textName);
+        userDP = findViewById(R.id.dp);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        updateUI(user);
+    }
+
+    private void updateUI(FirebaseUser myUserObj) {
+        t1.setText(myUserObj.getEmail());
+        t2.setText(myUserObj.getDisplayName());
+
+        Picasso.with(ProfileActivity.this)
+                .load(user.getPhotoUrl())
+                .networkPolicy(NetworkPolicy.OFFLINE)
+                .placeholder(R.color.colorAccent)
+                .into(userDP, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError() {
+                        Picasso.with(ProfileActivity.this)
+                                .load(user.getPhotoUrl())
+                                .placeholder(R.color.colorAccent)
+                                .into(userDP);
+                    }
+                });
+    }
+
+    public void signout(View view) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        LoginManager.getInstance().logOut();
+        startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+        finish();
+    }
+}
